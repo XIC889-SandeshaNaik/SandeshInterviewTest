4dc77a5ea2c76b15eed42e6c3e3ffab1bb9c2b4b
XIC889-SandeshaNaik <Sandesha.Naik@xebia.com>
2022-08-27 11:13:24 +0530

    Project setup

--- payment-service-main/src/main/java/com/xebia/interview/ps/repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.xebia.interview.ps.repository;
-
-import com.xebia.interview.ps.domain.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
